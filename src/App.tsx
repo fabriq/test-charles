2e81c6c081eaeff3b6e8742c39286e52f962092c
Yacine Hmito <yacinehmito@users.noreply.github.com>
2022-08-19 12:08:44 +0200

    Get positions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import './App.css'
+import { DraggableCore } from "react-draggable";
+import "./App.css";
+
+interface Paragraph {
+  id: number;
+  value: string;
+}
 
 function App() {
-  const paragraphes: string[] = [
-      "toto",
-      "tata",
-      "titi",
-      "tutu"
+  const paragraphes: Paragraph[] = [
+    { id: 1, value: "toto" },
+    { id: 2, value: "tata" },
+    { id: 3, value: "titi" },
+    { id: 4, value: "tutu" },
   ];
+
+  const ys: Map<number, number> = new Map();
+  console.log(ys);
 
   return (
     <div className="App">
-      {paragraphes.map(paragraphe =>
-          <div>{paragraphe}</div>)}
+      <div>
+        {paragraphes.map((paragraphe) => (
+          <DraggableCore
+            // onDrag={(e) => console.log("drag", e)}
+            onStart={(e) => console.log("start", e)}
+            onStop={(e) => console.log("stop", e)}
+          >
+            <div
+              ref={(ref) => {
+                ys.set(paragraphe.id, ref?.getBoundingClientRect().y!);
+              }}
+            >
+              {paragraphe.value}
+            </div>
+          </DraggableCore>
+        ))}
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
